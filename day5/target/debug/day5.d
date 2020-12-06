40768c36cd9d2b7759cdab433ea494eee31d8d10
Bas Dusee <basdusee@hotmail.com>
2020-12-06 14:55:35 +0100

    deleted target dir for day 5

--- day5/target/debug/day5.d
+++
@@ -1,1 +0,0 @@
-/home/basd/GIT/Rust/AdventOfCode2020/day5/target/debug/day5: /home/basd/GIT/Rust/AdventOfCode2020/day5/src/main.rs
--- day5/target/debug/deps/day5-0ae4fa696cffe254.d
+++
@@ -1,5 +0,0 @@
-/home/basd/GIT/Rust/AdventOfCode2020/day5/target/debug/deps/day5-0ae4fa696cffe254: src/main.rs
-
-/home/basd/GIT/Rust/AdventOfCode2020/day5/target/debug/deps/day5-0ae4fa696cffe254.d: src/main.rs
-
-src/main.rs:
